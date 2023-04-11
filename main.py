f3a81e64a9aefa051216d0415f4143e3ba6f9e8e
hamolicious <kickfliptv8@gmail.com>
2023-04-11 15:32:41 +0100

    fixed extruder expeling filament after purging

--- main.py
+++ main.py
@@ -38,6 +38,30 @@ with open(os.path.join(cwd, file), 'r') as f:
 with open(os.path.join(this_dir, 'swap.gcode'), 'r') as f:
 	inserted_gcode = f.read()
 
+index = file_data.index(f';LAYER:{layer}')
+
+# Backtrack until you find G1 command with E steps (I know its ugly, bite me)
+i = index
+while True:
+	i -= 1
+	if file_data[i:i+2].startswith('G1'):
+		command = ''
+		j = 0
+		while True:
+			if file_data[i+j] == '\n':
+				break
+			command += file_data[i+j]
+			j += 1
+
+		if 'e' not in command.lower():
+			continue
+		e_steps = command[command.lower().index('e')+1::].split(' ')[0]
+
+		if not ('.' in e_steps and e_steps.replace('.', '').isdigit()):
+			print(f'[ERROR] Shoot, something went wrong, "{e_steps}" is not a valid value for E steps')
+
+		break
+
 home_code = 'G28              ; Home printer' if home else ''
 inserted_gcode = inserted_gcode.format(
 	dump_fil_f=settings.DUMP_FILAMENT_F,
@@ -45,9 +69,8 @@ inserted_gcode = inserted_gcode.format(
 	purge_len=settings.PURGE_LENGTH,
 	dump_len=settings.DUMP_LENGTH,
 	home=home_code,
+	old_e_steps=e_steps,
 )
-
-index = file_data.index(f';LAYER:{layer}')
 
 file_data = file_data[:index:] + inserted_gcode + file_data[index::]
 
--- swap.gcode
+++ swap.gcode
@@ -2,23 +2,25 @@
 ; =============== ;
 ;  FILAMENT SWAP  ;
 ; =============== ;
-G91              ; Relative positioning
+G91               ; Relative positioning
 
-G1 Z5            ; Move head up
-G1 E-{dump_len} F{dump_fil_f}    ; Dump the filament out
+G1 Z5             ; Move head up
+G1 E-{dump_len} F{dump_fil_f}     ; Dump the filament out
 
-G90              ; Absolute positioning
+G90               ; Absolute positioning
 
-G1 X0 Y0 F5000   ; Move over to the side
-M0               ; Pause
+G1 X0 Y0 F5000    ; Move over to the side
+M0                ; Pause
 
-G91              ; Relative positioning
+G91               ; Relative positioning
 
-G1 E{purge_len} F{purge_f}     ; Purge old filament
-G1 Z-5           ; Move head down
+G1 E{purge_len} F{purge_f}      ; Purge old filament
+G1 Z-5            ; Move head down
 
-G90              ; Absolute positioning
+G90               ; Absolute positioning
 {home}
+G92 E{old_e_steps} ; Reset extruder back to the previous E steps
+
 ; ================ ;
 ;  CONTINUE PRINT  ;
 ; ================ ;
